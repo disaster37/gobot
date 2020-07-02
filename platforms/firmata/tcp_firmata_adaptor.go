5aed348fad6feb544f390a87654913168fc17765
disaster <linuxworkgroup@hotmail.com>
2020-07-02 19:39:06 +0200

    Add keepalive

--- platforms/firmata/tcp_firmata_adaptor.go
+++ platforms/firmata/tcp_firmata_adaptor.go
@@ -3,6 +3,7 @@ package firmata
 import (
 	"io"
 	"net"
+	"time"
 
 	"gobot.io/x/gobot"
 )
@@ -13,19 +14,32 @@ type TCPAdaptor struct {
 	*Adaptor
 }
 
-func connect(address string) (io.ReadWriteCloser, error) {
-	return net.Dial("tcp", address)
+func connect(address string, timeout time.Duration) (io.ReadWriteCloser, error) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		return nil, err
+	}
+	conn.SetKeepAlive(true)
+	conn.SetKeepAlivePeriod(time.Second * 30)
+
+	return conn, err
 }
 
 // NewTCPAdaptor opens and uses a TCP connection to a microcontroller running
 // WiFiFirmata
 func NewTCPAdaptor(args ...interface{}) *TCPAdaptor {
 	address := args[0].(string)
+	timeout := args[1].(time.Duration)
 
 	a := NewAdaptor(address)
 	a.SetName(gobot.DefaultName("TCPFirmata"))
 	a.PortOpener = func(port string) (io.ReadWriteCloser, error) {
-		return connect(port)
+		return connect(port, timeout)
 	}
 
 	return &TCPAdaptor{
